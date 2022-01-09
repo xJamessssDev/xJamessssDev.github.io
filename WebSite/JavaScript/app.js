36c0fa9984a0078dfa0e5c4a21d8fa190c8abf2d
xJamessssDev <96695399+xJamessssDev@users.noreply.github.com>
2022-01-09 15:36:13 +0100

    Add files via upload

---
+++ WebSite/JavaScript/app.js
@@ -0,0 +1,16 @@
+const tabs = document.querySelector('[data-tab-target]')
+const tabContents = document.querySelector('[data-tab-content]')
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.tabTarget)
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('active')
+        })
+        tabs.forEach(tab => {
+            tab.classList.remove('active')
+        })
+        tab.classList.add('active')
+        target.classList.add('active')
+    })
+})
